695b23a3e0b90d17cba17d39d0daa56a0593bad6
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2024-07-17 17:52:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,25 @@ import express from "express"
 import { Connectdb } from "./config/connectDB.js"
 import DataRoute from "./Route/DataRoute.js"
 configDotenv()
+import path from 'path'
+import { fileURLToPath } from "url";
 //dbconnection
 Connectdb()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 const app = express() 
 //middleware 
 app.use(cors())
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname, './client/build')))
 //route
 app.use("/api/v1/data",DataRoute)
+
+app.use("*",function(req,res){
+     res.sendFile(path.join(__dirname,"./client/build/index.html"));
+});
 //port 
 const PORT = process.env.PORT ||8080
 //app/listen
